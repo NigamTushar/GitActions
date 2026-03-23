bebee5f0dbf87ee9c2e47e7262b1949f5eda56a3
NigamTushar <96563330+NigamTushar@users.noreply.github.com>
2026-03-23 17:32:16 +0530

    Merge pull request #1 from NigamTushar/home
    added home child

---
+++ lib/home_child.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeChild extends StatelessWidget {
+  const HomeChild({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
